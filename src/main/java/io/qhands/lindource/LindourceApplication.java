06d571dcf4f3c04806c3d49013d7c333349d18b1
Davith <gvasaliadavit3@gmail.com>
2026-04-27 14:32:23 +0400

    Initial Spring Boot project setup for QHANDS-01

---
+++ src/main/java/io/qhands/lindource/LindourceApplication.java
@@ -0,0 +1,13 @@
+package io.qhands.lindource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/qhands/lindource/LindourceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.qhands.lindource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
